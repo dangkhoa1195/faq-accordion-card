383dffc7cb917e00e870e4e94223c4f3e6e341f9
dangkhoa1195 <ngdangkhoa1195@gmail.com>
2022-11-29 14:32:08 +0700

    update html & css for mobile design

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@ const questionsElement = document.querySelectorAll('.card__question');
 
 iconsElement.forEach(icon =>
   icon.addEventListener('click', event => {
-    event.target.classList.toggle('close');
-    event.target.parentNode.nextElementSibling.classList.toggle('hide');
+    const currentIcon = event.target;
+    currentIcon.classList.toggle('close');
+    currentIcon.parentNode.nextElementSibling.classList.toggle('hide');
+    currentIcon.previousElementSibling.classList.toggle('font-weight-700');
   })
 );
 questionsElement.forEach(icon =>
   icon.addEventListener('click', event => {
-    event.target.nextElementSibling.classList.toggle('close');
-    event.target.parentNode.nextElementSibling.classList.toggle('hide');
+    const currentQuestion = event.target;
+    currentQuestion.nextElementSibling.classList.toggle('close');
+    currentQuestion.parentNode.nextElementSibling.classList.toggle('hide');
+    currentQuestion.classList.toggle('font-weight-700');
   })
 );
